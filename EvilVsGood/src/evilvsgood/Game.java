f6b45c2a2dd7414bc4a4e56ae4c1ead3b3407a62
Connor <connorcoale@gmail.com>
2016-05-17 11:49:46 -0400

    added Ledyar's files into the project

---
+++ EvilVsGood/src/evilvsgood/Game.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evg;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author TuckerHase
+ */
+public class Game {
+    private ArrayList <Scenario> s;
+    private int score;
+    
+    public Game (ArrayList <Scenario> r){
+        s = r;
+        score = 0;
+    }
+    
+    public Game (){
+        s = new ArrayList<Scenario>();
+        score = 0;
+    }
+    
+    public int getScore(){
+        return score;
+    }
+    
+    public void addScore(int amount){
+        score += amount;
+    }
+    
+    public void addScenario(Scenario sce){
+        s.add(sce);
+    }
+    
+    public int getRemainingScenarios (){
+        return s.size();
+    }
+    
+    public Scenario getRandomScenario(){
+        int r = (int) (Math.random()* s.size());
+        Scenario sc = s.get(r);
+        s.remove(r);
+        return sc;
+    }
+    
+    public boolean isGood(){
+    if (score >= 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+    
+    
+}
